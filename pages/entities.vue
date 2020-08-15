ca7aadae109859293af94bae7dcaca4a57c27e94
Edwin Groenendaal <edwin.groenendaal@condaal.com>
2020-08-15 11:21:19 +0100

    Added a quick entities page

---
+++ pages/entities.vue
@@ -0,0 +1,86 @@
+<template>
+  <CFlex background-color="gray.50" flex="1 0" direction="row">
+    <CBox :width="$chakra.theme.container" mx="auto" background-color="white">
+      <CFlex p="4" align="center">
+        <CHeading as="h2" font-size="4xl">
+          Entities
+        </CHeading>
+        <CButton ml="auto">
+          Add entity
+        </CButton>
+      </CFlex>
+      <CBox as="section" p="4">
+        <CStack spacing="4">
+          <CBox v-if="$fetchState.pending" py="2">
+            Retrieving entities
+          </CBox>
+          <CPseudoBox v-for="entity in entities" v-else :key="entity.id">
+            <CStack is-inline spacing="4" align="center">
+              <CText font-weight="500">
+                {{ entity.display_name }}
+              </CText>
+              <CBadge font-size="md" variant="outline" rounded="4px">
+                Revision {{ entity.revision.revision }}
+              </CBadge>
+              <CBadge v-for="tag in entity.tags" :key="tag">
+                {{ tag }}
+              </CBadge>
+              <CStack is-inline spacing="4" ml="auto" align="center">
+                <CText>
+                  {{ entity.revision.updated_by }}
+                </CText>
+                <CText>5 days ago</CText>
+                <CFlex
+                  font-weight="700"
+                  color="white"
+                  background-color="green.600"
+                  rounded="0.25rem"
+                  justify="center"
+                  align="center"
+                  h="10"
+                  px="4"
+                >
+                  {{ entity.revision.status }}
+                </CFlex>
+              </CStack>
+            </CStack>
+          </CPseudoBox>
+        </CStack>
+      </CBox>
+    </CBox>
+  </CFlex>
+</template>
+<script>
+import { CBadge, CBox, CButton, CFlex, CHeading, CPseudoBox, CStack, CText } from '@chakra-ui/vue'
+
+export default {
+  components: {
+    CBadge,
+    CBox,
+    CButton,
+    CFlex,
+    CHeading,
+    CPseudoBox,
+    CStack,
+    CText
+  },
+  async fetch () {
+    try {
+      const { $axios } = this.$nuxt.context
+      const { data } = await $axios.get(
+        'http://localhost:7071/api/entity/Teppa'
+      )
+      this.entities = data
+      console.log(this.entities)
+    } catch (err) {
+      console.log('ERR')
+      console.log(err)
+    }
+  },
+  data () {
+    return {
+      entities: []
+    }
+  }
+}
+</script>
