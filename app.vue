b7da95de5494f5c386e5290ec4e26c8bb3f141e0
Dharmatologist <44667896+Dharmatologist@users.noreply.github.com>
2023-03-17 08:00:11 +0000

    Cleaning up fonts

--- app.vue
+++ app.vue
@@ -1,27 +1,40 @@
 <script lang="ts" setup>
   // Comment to force a build 20220715.01
 
-import "./assets/css/tailwind.css"
-
-  import {Bars3Icon} from "@heroicons/vue/24/outline"
+  import "./assets/css/tailwind.css"
 
   useHead({
     title: "Rulebox Studio",
     // or, instead:
     // titleTemplate: (title) => `My App - ${title}`,
     meta: [{name: "description", content: "Rule Management for everyone"}],
-    link: [{rel: "icon", type: "image/x-icon", href: "/favicon.ico"}],
+    link: [
+      {rel: "icon", type: "image/x-icon", href: "/favicon.ico"},
+      {rel: "preconnect", href: "https://fonts.googleapis.com"},
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossorigin: "true",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap",
+      },
+    ],
   })
-
 </script>
 
 <template>
-  <div class="flex h-screen overflow-hidden bg-desaturated-100 dark:bg-desaturated-900">
-    <div class="fixed inset-x-0 top-[-100px] h-[295px] bg-gradient-to-r blur-[100px]  from-[rgba(255,170,0,0.2)] via-[rgba(132,70,255,0.2)] to-[rgba(1,255,1,0.2)]  dark:from-[rgba(255,170,0,0.2)] dark:via-[rgba(132,70,255,0.2)] dark:to-[rgba(1,255,1,0.2)] dark:top-[-270px]"></div>
-    <div class="fixed inset-x-0 bottom-[-300px] h-[295px] bg-gradient-to-r blur-[100px]  from-[rgba(255,170,0,0.2)] via-[rgba(132,70,255,0.2)] to-[rgba(1,255,1,0.2)]  dark:from-[rgba(255,170,0,0.2)] dark:via-[rgba(132,70,255,0.2)] dark:to-[rgba(1,255,1,0.2)]"></div>
+  <div
+    class="flex h-screen overflow-hidden bg-desaturated-100 dark:bg-desaturated-900">
+    <div
+      class="fixed inset-x-0 top-[-100px] h-[295px] bg-gradient-to-r from-[rgba(255,170,0,0.2)] via-[rgba(132,70,255,0.2)] to-[rgba(1,255,1,0.2)] blur-[100px] dark:top-[-270px] dark:from-[rgba(255,170,0,0.2)] dark:via-[rgba(132,70,255,0.2)] dark:to-[rgba(1,255,1,0.2)]"></div>
+    <div
+      class="fixed inset-x-0 bottom-[-300px] h-[295px] bg-gradient-to-r from-[rgba(255,170,0,0.2)] via-[rgba(132,70,255,0.2)] to-[rgba(1,255,1,0.2)] blur-[100px] dark:from-[rgba(255,170,0,0.2)] dark:via-[rgba(132,70,255,0.2)] dark:to-[rgba(1,255,1,0.2)]"></div>
     <SidebarMobile></SidebarMobile>
     <SidebarDesktop></SidebarDesktop>
-    <main class="relative z-0 flex w-0 flex-1 flex-col overflow-y-auto focus:outline-none">
+    <main
+      class="relative z-0 flex w-0 flex-1 flex-col overflow-y-auto focus:outline-none">
       <NuxtPage />
     </main>
   </div>
@@ -39,7 +52,3 @@ import "./assets/css/tailwind.css"
 
   -->
 </template>
-
-<style lang="postcss">
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap');
-</style>
